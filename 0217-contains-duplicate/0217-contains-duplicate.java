4214278159b9905968ef5f6268d9082a14d9f793
Raj3493 <161839371+Raj3493@users.noreply.github.com>
2026-03-13 13:15:21 +0530

    Time: 15 ms (65.16%), Space: 93.5 MB (46.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -6,8 +6,10 @@ class Solution {
         for ( int n : nums){
             if (seen.contains(n)){
                 return true;
+            }else{
+                seen.add(n);
             }
-            seen.add(n);
+            
 
         }
         return false;
