899ca14e31f15fe4381290cda9fdf8d8b6400faf
Raj3493 <161839371+Raj3493@users.noreply.github.com>
2026-03-14 09:10:06 +0530

    Time: 3 ms (92.17%), Space: 44 MB (97.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,18 +3,21 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-        
-        int[] count=new int[26];
-        for(int j=0;j<s.length();j++){
-            count[s.charAt(j)-'a']++;
-            count[t.charAt(j)-'a']--;
+
+        int[] count = new int[26];
+
+        for (int j = 0; j < s.length(); j++) {
+            count[s.charAt(j) - 'a']++;
         }
 
-        for ( int v : count){
-            if ( v != 0 ){
+        for (int j = 0; j < t.length(); j++) {
+            int idx = t.charAt(j) - 'a';
+            count[idx]--;
+            if (count[idx] < 0) {
                 return false;
             }
         }
+
         return true;
         
 
