e50fc20d92910899b512914a2f806d8a2065878a
Raj3493 <161839371+Raj3493@users.noreply.github.com>
2026-03-29 19:41:29 +0530

    Time: 3 ms (87.35%), Space: 43.4 MB (50.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stck=new ArrayDeque<>();
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c=='(' || c=='{' || c=='['){
-                stck.push(c);
-            }
-            else{
-                if(stck.isEmpty()){
+        Deque<Character> stack=new ArrayDeque<>();
+        // for(int i=0;i<s.length();i++){
+        //     char c=s.charAt(i);
+        //     if(c=='(' || c=='{' || c=='['){
+        //         stck.push(c);
+        //     }
+        //     else{
+        //         if(stck.isEmpty()){
+        //             return false;
+        //         }
+        //         char top=stck.pop();
+        //         if (c == ')' && top != '(') return false;
+        //         if (c == '}' && top != '{') return false;
+        //         if (c == ']' && top != '[') return false;
+        //     }
+        // }
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else {
+                if (stack.isEmpty() || stack.pop() != c) {
                     return false;
                 }
-                char top=stck.pop();
-                if (c == ')' && top != '(') return false;
-                if (c == '}' && top != '{') return false;
-                if (c == ']' && top != '[') return false;
             }
         }
-        return stck.isEmpty();
+        return stack.isEmpty();
         
     }
         
